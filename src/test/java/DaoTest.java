ec40e01bc4ea4a722e9fe2b2aa95ee40d5942d21
sleepbear669 <sleepbear669@gmail.com>
2015-04-03 11:16:10 +0900

    step 3 refectoring make UserDao method getConnection next request produce NUserDao ,DUserDao

--- src/test/java/DaoTest.java
+++ src/test/java/DaoTest.java
@@ -13,45 +13,33 @@ public class DaoTest {
     private User user;
     @Before
     public void setUser() {
-        user = new User("sleepbear", "gom", "gom0119!1");
-    }
-
-    @Test
-    public void testGetAndAddUser() throws Exception {
-        UserDao userDao = new UserDao();
-        User user = new User();
-        String id = String.valueOf(new Random().nextInt()/100);
-        String name = "gom";
-        String password = "gom0119!1";
-        user.setId(id);
-        user.setName(name);
-        user.setPassword(password);
-
-        userDao.add(user);
-
-        User userGet = userDao.get(id);
-
-        assertThat( userGet.getId(), is( user.getId() ));
-        assertThat( userGet.getName(), is(user.getName() ));
-        assertThat(userGet.getPassword(), is(user.getPassword()));
-
+        user = new User("sleepbear1", "gom", "gom0119!1");
     }
 
     @Test(expected = SQLException.class)
     public void testDeleteUser() throws Exception {
         UserDao userDao = new UserDao();
-        User user = new User("test1", "gom", "gom0119!1");
-        userDao.add(user);
         userDao.delete(user.getId());
-
         assertNull(userDao.get(user.getId()));
+    }
+
+    @Test
+    public void testGetAndAddUser() throws Exception {
+        UserDao userDao = new UserDao();
+
+        userDao.add(user);
+        User userGet = userDao.get(user.getId());
+
+        assertThat(userGet.getId(), is(user.getId()));
+        assertThat(userGet.getName(), is(user.getName()));
+        assertThat(userGet.getPassword(), is(user.getPassword()));
+
     }
 
     private class UserDao {
 
         public User get(String id) throws ClassNotFoundException, SQLException {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "gom0119!1");
+            Connection connection = getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE id = ?");
             preparedStatement.setString(1, id);
 
@@ -69,9 +57,13 @@ public class DaoTest {
             return user;
         }
 
-        public void add(User user) throws ClassNotFoundException, SQLException {
+        private Connection getConnection() throws ClassNotFoundException, SQLException {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "gom0119!1");
+            return DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "gom0119!1");
+        }
+
+        public void add(User user) throws ClassNotFoundException, SQLException {
+            Connection connection = getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users(id, name, password) VALUES (? , ?, ?)");
             preparedStatement.setString(1, user.getId());
             preparedStatement.setString(2, user.getName());
@@ -83,8 +75,7 @@ public class DaoTest {
         }
 
         public void delete(String id) throws ClassNotFoundException, SQLException {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "gom0119!1");
+            Connection connection = getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
             preparedStatement.setString(1, id);
             preparedStatement.executeUpdate();
