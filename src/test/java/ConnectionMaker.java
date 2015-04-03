d623259ff3f10018c81cf3df8818a473e80dd7a8
sleepbear669 <sleepbear669@gmail.com>
2015-04-03 14:47:50 +0900

    remove ConnectionMaker && NConnectionMaker

--- src/test/java/ConnectionMaker.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-
-/**
- * Created by sleepbear on 2015-04-03.
- */
-public interface ConnectionMaker {
-    public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
